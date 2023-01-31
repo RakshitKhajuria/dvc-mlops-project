5ff4182beef69206fcebda48d300255932eff1ea
Ryzxxl <rakshitraina1234@gmail.com>
2023-01-31 19:09:32 +0530

    first stage final push

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,44 +1,11 @@
-#from src.utils.all_utils import read_yaml, create_directory
+from src.utils.all_utils import read_yaml, create_directory
 #IT was throwing an error importing my own modules so i added the functions that i created in utils here
 import argparse
 import pandas as pd
-'''
-********************************************************************
-********************************************************************
-'''
+
 import yaml
 import os
 import json
-
-
-def read_yaml(path_to_yaml: str) -> dict:
-    with open(path_to_yaml) as yaml_file:
-        content = yaml.safe_load(yaml_file)
-
-    return content
-
-
-def create_directory(dirs: list):
-    for dir_path in dirs:
-        os.makedirs(dir_path, exist_ok=True)
-        print(f"directory is created at {dir_path}")
-
-
-def save_local_df(data, data_path, index_status=False):
-    data.to_csv(data_path, index=index_status)
-    print(f"data is saved at {data_path}")
-
-
-def save_reports(report: dict, report_path: str, indentation=4):
-    with open(report_path, "w") as f:
-        json.dump(report, f, indent=indentation)
-    print(f"reports are saved at {report_path}")
-
-'''
-********************************************************************
-********************************************************************
-'''
-
 
 def get_data(config_path):
     config = read_yaml(config_path)
